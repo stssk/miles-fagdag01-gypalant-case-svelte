b0278941d6f229fb6187d1f6d5c4dc37ef3d201b
Petter Thorgersen <petter.thorgersen@miles.no>
2021-10-08 15:02:37 +0200

    path with url

---
+++ src/routes/customize/[name].svelte
@@ -0,0 +1,47 @@
+<script context="module" lang="ts">
+
+	// see https://kit.svelte.dev/docs#loading
+	export function load({ page }) {
+		return {
+			props: {
+				characterName: page.params.name,
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import { browser, dev } from '$app/env';
+
+	export let characterName;
+	
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+	console.log();
+
+</script>
+
+<svelte:head>
+	<title>Customizer</title>
+</svelte:head>
+
+<div class="content">
+    Customize {characterName}!
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
