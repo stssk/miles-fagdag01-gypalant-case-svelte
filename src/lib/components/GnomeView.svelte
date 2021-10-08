6de91cbf641c123af5fe94fa03c3662cc4223e66
Haavard Haug <haavardhaug@gmail.com>
2021-10-08 11:43:44 +0200

    Use grid with areas

--- src/lib/components/GnomeView.svelte
+++ src/lib/components/GnomeView.svelte
@@ -1,14 +1,45 @@
 <script lang="ts">
     import type { Gnome } from "$lib/types";
+    import RoundBody from "./items/GnomeBodies/RoundBody.svelte";
     import TriangleHat from "./items/Hats/TriangleHat.svelte";
 
     export let gnome: Gnome;
 </script>
 
 <div class="gnome-view">
-    <TriangleHat></TriangleHat>
+    <!-- <div class="gnome-hat">
+        <TriangleHat></TriangleHat>
+    </div> -->
+    <div class="gnome-head">
+        <RoundBody></RoundBody>
+    </div>
+    <!-- <div class="facial-hair"></div> -->
+    <div class="gnome-body">
+        <TriangleHat></TriangleHat>
+    </div>
+    <div class="gnome-feet">
+        <TriangleHat></TriangleHat>
+    </div>
 </div>
 
 <style>
+    .gnome-view {
+        display: grid;
+        grid:
+        "tl head tr" auto
+        "arml torso armr" auto
+        "bl feet br" auto;
+    }
 
+    .gnome-head {
+        grid-area: head;
+    }
+
+    .gnome-body {
+        grid-area: torso;
+    }
+
+    .gnome-feet {
+        grid-area: feet;
+    }
 </style>
--- src/lib/components/items/GnomeBodies/RoundBody.svelte
+++ src/lib/components/items/GnomeBodies/RoundBody.svelte
@@ -1,6 +1,14 @@
-<div class="round-body">
-</div>
+<div class="round-body" />
 
 <style>
-    
+	.round-body {
+		box-sizing: border-box;
+		position: relative;
+		display: block;
+		transform: scale(var(--ggs, 1));
+		width: 115px;
+		height: 115px;
+		border: 3px solid;
+		border-radius: 100px;
+	}
 </style>
