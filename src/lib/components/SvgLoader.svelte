53a7621376b0706de65a5cf8c7775de524da1bce
Stian Sanden Skauge <stian@skauge.org>
2021-10-08 15:13:27 +0200

    Adding more images

---
+++ src/lib/components/SvgLoader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let svgImage;
+	export let alt;
+	const imageSource = `/images/${svgImage}.svg`;
+</script>
+
+<div class="svg-loader">
+	<img class="svg-image" src={imageSource} {alt} />
+</div>
+
+<style>
+	.svg-image {
+		width: 100px;
+	}
+</style>
---
+++ src/routes/test.svelte
@@ -0,0 +1,8 @@
+<script>
+	import SvgLoader from '$lib/components/SvgLoader.svelte';
+</script>
+
+<div class="test">
+	<SvgLoader svgImage="heads/face" alt="a gnomish face" />
+	<SvgLoader svgImage="bodies/body" alt="a gnomish body" />
+</div>
