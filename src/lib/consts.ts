bd88eb447aa08f3ef4e1c17ba2ecf471a0400b69
Stian Sanden Skauge <stian@skauge.org>
2021-10-08 15:58:39 +0200

    Types

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,1 +1,23 @@
 export const colors: string[] = ['Red', 'Green', 'Purple', 'Pink', 'Gray', 'Yellow'];
+
+export const heads = [
+    {
+        path: "heads/head0",
+        description: "A gnomish head"
+    }
+];
+
+export const bodies = [
+    {
+        path: "bodies/body0",
+        description: "A gnomish body"
+    },
+    {
+        path: "bodies/body1",
+        description: "A gnomish body"
+    },
+    {
+        path: "bodies/body2",
+        description: "A gnomish body"
+    },
+];
