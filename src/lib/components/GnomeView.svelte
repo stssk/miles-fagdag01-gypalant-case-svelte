46bf6a94fc1b7700fde267d3db8988bd9503b189
Stian Sanden Skauge <stian@skauge.org>
2021-10-08 15:44:33 +0200

    GnomeView using svg

--- src/lib/components/GnomeView.svelte
+++ src/lib/components/GnomeView.svelte
@@ -1,45 +1,46 @@
 <script lang="ts">
-    import type { Gnome } from "$lib/types";
-    import RoundBody from "./items/GnomeBodies/RoundBody.svelte";
-    import TriangleHat from "./items/Hats/TriangleHat.svelte";
+	import type { Gnome } from '$lib/types';
+	import RoundBody from './items/GnomeBodies/RoundBody.svelte';
+	import TriangleHat from './items/Hats/TriangleHat.svelte';
+	import SvgLoader from './SvgLoader.svelte';
 
-    export let gnome: Gnome;
+	export let gnome: Gnome;
 </script>
 
 <div class="gnome-view">
-    <!-- <div class="gnome-hat">
+	<!-- <div class="gnome-hat">
         <TriangleHat></TriangleHat>
     </div> -->
-    <div class="gnome-head">
-        <RoundBody></RoundBody>
-    </div>
-    <!-- <div class="facial-hair"></div> -->
-    <div class="gnome-body">
-        <TriangleHat></TriangleHat>
-    </div>
-    <div class="gnome-feet">
-        <TriangleHat></TriangleHat>
-    </div>
+	<div class="gnome-head">
+		<SvgLoader svgImage="heads/head0" alt="A gnomish head" />
+	</div>
+	<!-- <div class="facial-hair"></div> -->
+	<div class="gnome-body">
+		<SvgLoader svgImage="bodies/body0" alt="A gnomish body" />
+	</div>
+	<div class="gnome-feet">
+		<TriangleHat />
+	</div>
 </div>
 
 <style>
-    .gnome-view {
-        display: grid;
-        grid:
-        "tl head tr" auto
-        "arml torso armr" auto
-        "bl feet br" auto;
-    }
+	.gnome-view {
+		display: grid;
+		grid:
+			'tl head tr' auto
+			'arml torso armr' auto
+			'bl feet br' auto;
+	}
 
-    .gnome-head {
-        grid-area: head;
-    }
+	.gnome-head {
+		grid-area: head;
+	}
 
-    .gnome-body {
-        grid-area: torso;
-    }
+	.gnome-body {
+		grid-area: torso;
+	}
 
-    .gnome-feet {
-        grid-area: feet;
-    }
+	.gnome-feet {
+		grid-area: feet;
+	}
 </style>
